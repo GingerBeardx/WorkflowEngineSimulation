cae605ebba3a2b89ae75152fb9f6e3c3bc6613c5
GingerBeardx <ecryptographer@gmail.com>
2020-11-05 06:16:55 -0700

    Add project files.

---
+++ WorkflowEngineSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkflowEngineSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
