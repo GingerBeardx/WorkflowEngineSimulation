36fcb4a63ef2a7f3440069abfc9f4a538988886f
GingerBeardx <ecryptographer@gmail.com>
2020-11-05 06:41:01 -0700

    Activity Engine set up with list of activities.

--- WorkflowEngineSimulation/WorkflowEngine.cs
+++ WorkflowEngineSimulation/WorkflowEngine.cs
@@ -6,5 +6,19 @@ namespace WorkflowEngineSimulation
 {
     class WorkflowEngine
     {
+        public List<IActivity> Activities { get; set; } = new List<IActivity>();
+
+        public WorkflowEngine()
+        {
+            Activities.Add(new VideoUpload("My First Video", 15));
+            Activities.Add(new VideoEncoder("My first video", "Standard"));
+            Activities.Add(new SendEmail("Video Encoding Ready", "echo@nowhere.com", "Your video has been recieved and is being encoded."));
+            Activities.Add(new DbStatusUpdate("ConnectMe"));
+
+            foreach (var activity in Activities)
+            {
+                activity.Execute();
+            }
+        }
     }
 }
